6ba899af0a53a8ad8913038917f942edf6df11d8
Marius Fersigan <marius.fersigan@laifscience.com>
2018-08-15 06:25:05 +0300

    update boxes

--- serving.py
+++ serving.py
@@ -51,8 +51,8 @@ def predict():
 
             for cat, score, bounds in results:
                 x, y, w, h = bounds
-                cv2.rectangle(image, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), int(y + h / 2)), (255, 0, 0), thickness=2)
-                cv2.putText(image,str(cat.decode("utf-8")),(int(x),int(y)),cv2.FONT_HERSHEY_DUPLEX,4,(0,0,255), thickness=2)                
+                cv2.rectangle(cv2_image, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), int(y + h / 2)), (255, 0, 0), thickness=2)
+                cv2.putText(cv2_image,str(cat.decode("utf-8")),(int(x),int(y)),cv2.FONT_HERSHEY_DUPLEX,4,(0,0,255), thickness=2)                
     
 
             buffered = BytesIO()
@@ -70,28 +70,24 @@ def objdetect():
     data = {"success": False}
 
     # ensure an image was properly uploaded to our endpoint
-    if flask.request.method == "POST":       
+    if received.get("image"):
+        # read the image in PIL format            
+        image = b64decode(received["image"])
+        image = pil.open(io.BytesIO(image)) 
+        cv2_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        darknet_img = Image(cv2_image) 
+
+        result = []
         
-        decoded = flask.request.data.decode("utf-8")
-        print ("decoding ready")
-        received =  json.loads(decoded)
+        results = model.detect(darknet_img)    
+
+        for cat, score, bounds in results:
+            detected_class = cat.decode('utf-8')
+            x, y, w, h = bounds
+            result.append({"class": detected_class, "score": float(score), "box": { "x": int(x), "y": int(y), "w":int(w), "h": int(h)}})
+
         
-        if received.get("image"):
-            # read the image in PIL format
-            image = b64decode(received["image"])
-            image = Image.open(io.BytesIO(image))           
-            
-
-            # preprocess the image and prepare it for classification
-            #image = prepare_image(image, target=(224, 224))
-
-            # classify the input image and then initialize the list
-            # of predictions to return to the client
-            with graph.as_default():
-                result = model.detect_image_no_draw(image)     
-
-            
-            return flask.jsonify(result)
+        return flask.jsonify(result)
         
     return flask.jsonify({"failure": "Not valid input", "received": str(flask.request.data)})
     
